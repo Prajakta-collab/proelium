fa6483ad981c757e61bccd6fcb1a3546e8a72146
Prajakta Badgujar <52710902+Prajakta-collab@users.noreply.github.com>
2021-10-11 12:27:08 +0530

    models and routes created

---
+++ models/useradmin.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UserAdminSchema = new Schema({
+    Firstname:{
+        type: String,
+        required: true
+    },
+    Middlename:{
+        type: String,
+        required: true
+    },
+    Lastname:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        required: true
+    },
+    department:{
+        type: String,
+        required: true
+    },
+     { timestamps: true }
+  });
+
+  module.exports = mongoose.model('useradmin', UserAdminSchema);
